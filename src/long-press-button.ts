1db495b05f825ecf6173f79b81c40181bd51a330
maaaashi <doyaseizin.7@gmail.com>
2024-03-13 23:52:10 +0900

    長押しボタン処理

--- src/long-press-button.ts
+++ src/long-press-button.ts
@@ -5,10 +5,33 @@ import { customElement } from 'lit/decorators.js'
 
 @customElement('long-press-button')
 export class LongPressButton extends LitElement {
+  #timeId: number = 0;
+
   render() {
     return html`
-      <button><slot></slot></button>
+      <button
+        @mousedown="${this.#handleMouseDown}"
+        @mouseup="${this.#handleMouseUp}"
+        @mouseleave="${this.#handleMouseLeave}"
+      >
+        <slot></slot>
+      </button>
     `
+  }
+  
+  #handleMouseDown() {
+    this.#timeId = setTimeout(() => {
+      // ここに長押しした後に実行したい関数を記述
+      alert('長押しが検出されました！');
+    }, 3000); // 3000ミリ秒 = 3秒
+  }
+
+  #handleMouseUp() {
+    clearTimeout(this.#timeId);
+  }
+
+  #handleMouseLeave() {
+    clearTimeout(this.#timeId);
   }
 }
 
