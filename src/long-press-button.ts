3cfff492e4c2c699a1f4ab7f6127ef7e2b3864af
maaaashi <doyaseizin.7@gmail.com>
2024-03-13 22:56:05 +0900

    ボタン設置

--- src/long-press-button.ts
+++ src/long-press-button.ts
@@ -1,19 +1,13 @@
 import { LitElement, html } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
+
+
 @customElement('long-press-button')
 export class LongPressButton extends LitElement {
   render() {
     return html`
-      <div>
-        test
-      </div>
+      <button><slot></slot></button>
     `
   }
 }
